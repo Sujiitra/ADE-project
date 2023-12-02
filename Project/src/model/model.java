84be92a63c161165068189eaada83594e771e1a8
Ikechukwu Daniel <123109606+thedanielike@users.noreply.github.com>
2023-12-02 18:17:49 -0500

    Add files via upload

---
+++ Project/src/model/model.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author danielike
+ */
+public class model {
+    private ImageIcon displayPic;
+
+    public ImageIcon getDisplayPic() {
+        return displayPic;
+    }
+
+    public void setDisplayPic(ImageIcon displayPic) {
+        this.displayPic = displayPic;
+    }
+    
+    
+}
